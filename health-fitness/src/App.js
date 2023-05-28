ef51a24ede3e1f5363daaab3e8555e950c46e5e8
jmckelvey786 <77301869+jmckelvey786@users.noreply.github.com>
2023-05-28 12:10:01 -0400

    started building main page look

--- health-fitness/src/App.js
+++ health-fitness/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
-import './App.css';
+import './components/main.css'
+import Footer from "./components/footer"
+import {BrowserRouter as Router} from "react-router-dom"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-color-darkblue VH-100 flex justify-content-center'>
+      <div><h1 className='color-offwhite'>Health & Fitness Application</h1></div>
+      
+      <Footer />
     </div>
   );
 }
---
+++ health-fitness/src/components/footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import {link} from "react-router-dom"
+import { useState } from "react";
+
+const Footer = () => {
+    return (
+        <div id="footer" className="visual-aid-border-green width-100">
+            <div className="visual-aid-border-green flex justify-content-center align-items-center border-radius-15 m10 bg-color-aqua">
+                <h1 className="text-align-center visual-aid-border-green">Footer</h1>
+            </div>
+        </div>
+    );
+};
+
+export default Footer;
